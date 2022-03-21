3e27fe14838a3b1b06e7b108276926fe04002310
Yulissa-wu <97188330+Yulissa-wu@users.noreply.github.com>
2022-03-22 07:49:15 +0800

    Update PyMongo.py

--- PyMongo.py
+++ PyMongo.py
@@ -5,8 +5,13 @@ from pymongo import MongoClient
 conn = MongoClient("mongodb://IP") 
 db = conn["DataBase_Name"]
 collection = db.Collection_Name
-for doc in collection.find():
-	print(doc)
-
-list_first_name = [ (Collection_Name.get('firstName') +' ' + Collection_Name.get('lastName') )for Collection_Name in collection.find()]
-print('list_first_name: {}'.format(list_first_name))
+for doc in collection.find() as fetch:
+    for row in fetch:
+    # print(row)  # fetch出來的結果是NoneType
+    row = dict(row)  # 先將依序取出的檔案指定成dict資料型態
+    # print(row)
+    with open('Collection.csv', 'a', newline='') as f_object:
+        writer_object = writer(f_object)
+        writer_object.writerow(row.values())
+    f_object.close()  # 將dict轉檔成csv檔並且儲存關閉檔案
+    
